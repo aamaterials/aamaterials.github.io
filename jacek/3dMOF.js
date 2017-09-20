e9163b1ad25fbcf2380b1880eb535ffbdca34126
Marcus <marcus.fantham@gmail.com>
2017-09-20 14:28:06 +0100

    Added Jacek's data
    Created a new MOF explorer for Jacek's data.

---
+++ jacek/3dMOF.js
@@ -0,0 +1,560 @@
+google.charts.load("current", {'packages':["corechart", "table", "controls"]});
+google.charts.setOnLoadCallback(init);
+
+// GLOBAL VARIABLES
+var dataTable, chart, options;
+var table, view;
+var tableView, filterView;
+var plotlyPlot;
+var colorSelector, sizeSelector, xSelect, ySelect;
+var minSizeText; var maxSizeText; var sizeTitle; var colorbartitle;
+var filterRows = [];
+var allRows = []; selectAllArray = [];
+var gettingPointsFromGraph = false;
+var reDraw = true; var relaying = false;
+var sizeAxisGroup = null;
+var pointIndex = -1;
+var twoD = true;
+var cmax = 4; var smax = 24;
+var hmax = 28; var vmax = 320; var zmax = 28;
+var currentSelection = null;
+
+// INITIAL FUNCTION
+function init(){
+  // This function runs when Google charts js is ready
+
+  plotlyPlot = document.getElementById('chart_div');
+
+  colorSelector = document.getElementById('colorSelect');
+  colorSelector.addEventListener('change', selectChange);
+
+  sizeSelector = document.getElementById('sizeSelect');
+  sizeSelector.addEventListener('change', selectChange);
+
+  xSelector = document.getElementById('xSelect');
+  xSelector.addEventListener('change', selectChange);
+
+  ySelector = document.getElementById('ySelect');
+  ySelector.addEventListener('change', selectChange);
+
+  zSelector = document.getElementById('zSelect');
+  zSelector.addEventListener('change', selectChange);
+
+  zDiv = document.getElementById('zDiv');
+
+  filterGraphButton = document.getElementById('filterGraphButton');
+
+  minSizeText = document.getElementById('sizeAxisMinText');
+  maxSizeText = document.getElementById('sizeAxisMaxText');
+  sizeTitle = document.getElementById('sizeAxisTitle');
+
+  axesLabels = ['Identifier', 'Void Fraction', 'Metal Density, g/cm\u00B3', 'Density, g/cm\u00B3',
+                  'PLD \u212B', 'Largest Cavity Diameter, \u212B', 'Volumetric Surface Area, m\u00B2/cm\u00B3',
+                   'Gravimetric Surface Area, m\u00b2/g', 'Pore Volume, cm\u00B3/g',
+                    'Qst CO, kJ/mol',	'Qst N2, kJ/mol',	'Qst difference, kJ/mol',
+                    'CO uptake at desorption, mmol/g',	'N2 uptake at desorption, mmol/g',
+                    'CO uptake at 40 bar, mmol/g',	'N2 uptake at 40 bar, mmol/g',	'CO uptake at 200 K, mmol/g',
+                    'N2 uptake at 200 K, mmol/g',	'CO uptake at 40 bar, 200 K, mmol/g',	'N2 uptake at 40 bar, 200 K, mmol/g',
+                    'Selectivity at desorption',	'PSA selectivity',	'TSA selectivity',	'PTSA selectivity',
+                    'PSA capacity, mmol/g',	'TSA working capacity, mmol/g',	'PTSA working capacity, mmol/g',
+                    'PSA regenerability, %',	'TSA regenerability, %',	'PTSA regenerability, %'
+                    ];
+
+shortLabels = ['ID', 'VF', 'Metal Density, g/cm\u00B3', 'Density, g/cm\u00B3',
+                'PLD \u212B', 'LCD, \u212B', 'VSA, m\u00B2/cm\u00B3',
+                 'GSA, m\u00b2/g', 'Pore Volume, cm\u00B3/g',
+                  'Qst CO, kJ/mol',	'Qst N2, kJ/mol',	'Qst diff., kJ/mol',
+                  'CO up (desorption), mmol/g',	'N2 up (desorption), mmol/g',
+                  'CO up (40 bar), mmol/g',	'N2 up (40 bar), mmol/g',	'CO up (200 K), mmol/g',
+                  'N2 up (200 K), mmol/g',	'CO up (40 bar), 200 K, mmol/g',	'N2 up (40 bar, 200 K), mmol/g',
+                  'Selectivity (desorption)',	'PSA selectivity',	'TSA selectivity',	'PTSA selectivity',
+                  'PSA WC, mmol/g',	'TSA WC, mmol/g',	'PTSA WC, mmol/g',
+                  'PSA regenerability, %',	'TSA regenerability, %',	'PTSA regenerability, %'
+                  ];
+
+	layout = {
+          font: {family: 'Open Sans'},
+          margin: {l: 50, r: 0, t:20, b:50},
+					scene: {
+						xaxis: {range: [0, 28], title: 'Uptake, mol/kg', titlefont: {family: 'PT Sans Narrow'}},
+						yaxis: {range: [0, 320], title: 'Uptake, cm\u00B3(STP)/cm\u00B3', titlefont: {family: 'PT Sans Narrow'}},
+            zaxis: {range: [0, 28], title: 'Deliverable, mol/kg', titlefont: {family: 'PT Sans Narrow'}},
+            aspectratio: {x: 3, y: 1, z: 1},
+            camera: {}
+					},
+          xaxis: {range: [0, 28], title: 'Uptake, mol/kg'},
+          yaxis: {range: [0, 320], title: 'Uptake, cm\u00B3(STP)/cm\u00B3'},
+          zaxis: {range: [0, 28], title: 'Deliverable, mol/kg'},
+          hovermode: 'closest'
+				};
+
+  // Get data table from browser cache if possible
+  var dataString = localStorage.getItem('jacekmofs');
+
+  if (dataString == null){
+    // Download data table if necessary
+    var rangeString = encodeURIComponent('range=A:AQ');
+
+    var query = new google.visualization.Query(
+      'https://docs.google.com/spreadsheets/d/17IfMKn7zs3coqOxG8c3dY12BI3QeSMoXY_4fFsoBHSg/gviz/tq?gid=0&headers=1&' + rangeString);
+    query.send(handleQueryResponse);
+
+  }else{
+    console.log('Loading table from local storage.');
+    var parsedData = JSON.parse(dataString);
+    dataTable = new google.visualization.DataTable(parsedData);
+    // Draw initial chart
+    initialiseChart();
+  }
+
+}
+
+function reloadMOFdata(){
+  // Set up query
+  var rangeString = encodeURIComponent('range=A:AQ');
+  var query = new google.visualization.Query(
+    'https://docs.google.com/spreadsheets/d/1BXpfTXTDeg_gcDEWyjUmx_SUkWzwkZDSLilBQaabPO0/gviz/tq?gid=0&headers=1&' + rangeString);
+  query.send(handleQueryResponse);
+  // Close help dialog
+  document.getElementById('help-modal').style.display='none';
+}
+
+// FIRST DRAW OF CHART
+function initialiseChart(){
+  for (i=0; i<dataTable.getNumberOfRows(); i++){
+    allRows.push(i);
+    selectAllArray.push({'row': i, 'column': null});
+  }
+
+  dash = new google.visualization.Dashboard(document.getElementById('dashboard'));
+  filterTable = new google.visualization.Table(document.getElementById('filterTable_div'));
+  graphFilterTable = new google.visualization.Table(document.getElementById('graph-filterTable_div'))
+
+  var stringFilter = new google.visualization.ControlWrapper({
+    'controlType': 'StringFilter',
+    'containerId': 'control1',
+    'options': {
+      'filterColumnIndex': 0
+    }
+  });
+  table = new google.visualization.ChartWrapper({
+    'chartType': 'Table',
+    'containerId': 'table_div',
+    'options': {'height': '100%', 'width': '100%'}
+  });
+  dash.bind(stringFilter, table);
+
+  // Set up views
+  tableView = new google.visualization.DataView(dataTable);
+  filterView = new google.visualization.DataView(dataTable);
+  graphFilterView = new google.visualization.DataView(dataTable);
+  view = new google.visualization.DataView(dataTable);
+
+  // Draw first chart
+  drawBubbleChart();
+}
+
+function drawBubbleChart(){
+  setAxisSize();
+  var xValue = parseInt(xSelector.value); var yValue = parseInt(ySelector.value);
+  var cValue = parseInt(colorSelector.value); var sValue = parseInt(sizeSelector.value);
+  var zValue = parseInt(zSelector.value);
+
+  columns = getColumns(xValue, yValue, cValue, sValue, zValue);
+
+  view.setColumns(columns);
+  if (filterRows.length > 0){
+    view.setRows(filterRows);
+  } else {
+    // Empty filter means plot all rows
+    view.setRows(allRows);
+  }
+
+  colorbarTitle = axesLabels[cValue];
+  layout.xaxis.title = axesLabels[xValue];
+  layout.yaxis.title = axesLabels[yValue];
+  layout.scene.xaxis.title = axesLabels[xValue];
+  layout.scene.zaxis.title = axesLabels[yValue]; // Note z is vertical axis in 3D view
+  layout.scene.yaxis.title = axesLabels[zValue]; // Note z is vertical axis in 3D view
+
+  sizeTitle.innerHTML = axesLabels[sValue];
+  maxSizeText.innerHTML = view.getColumnRange(4).max.toPrecision(4).replace(/0+$/, "").replace(/\.$/, "");
+  minSizeText.innerHTML = view.getColumnRange(4).min.toPrecision(3).replace(/0+$/, "").replace(/\.$/, "");
+
+  hmax = view.getColumnRange(1).max;
+	vmax = view.getColumnRange(2).max;
+  zmax = view.getColumnRange(5).max;
+
+  cmax = view.getColumnRange(3).max;
+  smax = view.getColumnRange(4).max;
+
+	drawPlotlyChart();
+
+}
+
+function drawPlotlyChart(){
+  var trace = {
+    x:columnToArray(1),
+    mode: 'markers',
+    text: columnToArray(0),
+    hoverinfo: "text",
+    marker: {
+      size: columnToArray(4),
+      sizeref: smax/20,
+      sizemin: 2,
+      line: {width: 0.0},
+      color: columnToArray(3),
+      colorbar: {title: colorbarTitle, titleside: 'right'},
+      cmin: 0,
+      cmax: cmax,
+      colorscale: 'Jet',
+      autocolorscale: false,
+      showscale: true,
+      opacity: 0.9
+    }
+  };
+
+  // Trace settings for 2D/3D
+  if (twoD){
+    trace.y = columnToArray(2);
+    trace.type = 'scatter';
+  } else {
+    trace.y = columnToArray(5);
+    trace.z = columnToArray(2);
+    trace.type = 'scatter3d';
+    layout.aspectratio = {x: 3, y: 1, z: 1};
+  }
+
+	var data = [trace];
+
+  // Draw annotation, if necessary
+  if (currentSelection == null){
+    layout.annotations = [];
+  } else {
+    var viewRow = view.getViewRowIndex(currentSelection);
+    var xLoc = view.getValue(viewRow, 1);
+    var yLoc = view.getValue(viewRow, 2);
+    if (twoD){
+      // Annotations don't work in 3D :(
+      var annoText = '<b>' + view.getValue(viewRow, 0) + '</b><br>' +
+        layout.xaxis.title + ': ' + view.getValue(viewRow, 1).toPrecision(4) + '<br>' +
+        layout.yaxis.title + ': ' + view.getValue(viewRow, 2).toPrecision(4) + '<br>' +
+        colorbarTitle + ': ' + view.getValue(viewRow, 3).toPrecision(4) + '<br>' +
+        sizeTitle.innerHTML + ': ' + view.getValue(viewRow, 4).toPrecision(4) + '<br>';
+      layout.annotations = [{x: xLoc, y: yLoc, text: annoText, showarrow: true,
+        arrowhead: 7, arrowsize:1, arrowwidth:2, ax: 140, ay: 10, align: 'right',
+        xref: 'x', yref: 'y', bgcolor: '#666666', opacity: 0.8, font: {color: 'white'}}];
+      }
+  }
+
+	if(reDraw){
+    layout.xaxis.range = [0, hmax];
+    layout.yaxis.range = [0, vmax];
+    layout.scene.xaxis.range = [0, hmax];
+  	layout.scene.zaxis.range = [0, vmax]; // Note z is vertical axis in 3D view
+    layout.scene.yaxis.range = [0, zmax];
+
+    layout.scene.camera = {center: {x: 0, y: 0, z: -0.1},
+                    eye: {x: 0.02, y: -2.2, z: 0.1}}; // up = 0 1 0 ?
+		Plotly.newPlot('chart_div', data, layout);
+
+    // Set up events
+    plotlyPlot.on('plotly_click', debounce(pointSelected, 200));
+    plotlyPlot.on('plotly_relayout', function(){relaying=true;});
+		reDraw = false;
+	}else{
+    if (!twoD){
+      layout.scene.camera = {center: {x: chart_div._fullLayout.scene._scene.camera.center[0],
+                                      y: chart_div._fullLayout.scene._scene.camera.center[1],
+                                      z: chart_div._fullLayout.scene._scene.camera.center[2]},
+                             eye:    {x: chart_div._fullLayout.scene._scene.camera.eye[0],
+                                      y: chart_div._fullLayout.scene._scene.camera.eye[1],
+                                      z: chart_div._fullLayout.scene._scene.camera.eye[2]}
+                          };
+    }
+		Plotly.animate('chart_div', {data: data}, {transition: {duration: 1000, easing: 'cubic-in-out' }});
+	}
+}
+
+// Convert Google chart column to Plotly array
+function columnToArray(columnIndex){
+  var outputArray = new Array(view.getNumberOfRows());
+  for (i=0; i<view.getNumberOfRows(); i++){
+    outputArray[i] = (view.getValue(i, columnIndex));
+  }
+  return outputArray;
+}
+
+function switch2D(){
+  reDraw = true;
+  twoD = !twoD;
+  zDiv.hidden = twoD;
+  filterGraphButton.disabled = !twoD;
+  drawBubbleChart();
+}
+
+// GET DATA TABLE
+function handleQueryResponse(response) {
+  if (response.isError()) {
+    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+
+  // Get data, and store it in localStorage
+  dataTable = response.getDataTable();
+  var dataString = JSON.stringify(dataTable);
+  localStorage['jacekmofs'] = dataString;
+  console.log('Saved remote MOF data to local storage.');
+  initialiseChart();
+}
+
+function setAxisSize(){
+  var sizeAxisGroup = document.getElementById('sizeAxisGroup').cloneNode(true);
+
+  var circ1 = document.getElementById('sizeAxisMinCirc');
+  var circ2 = document.getElementById('sizeAxisMaxCirc');
+  var tri   = document.getElementById('sizeAxisTriangle');
+
+  var boxWidth = document.documentElement.clientWidth - 30;
+  var xStart = 10;
+  var xEnd = xStart + boxWidth;
+
+  var textLength = 30;
+  var offset = 6;
+
+  var exampleText = document.querySelector('text.xtitle');
+  var fontSize = "14px";
+  if (exampleText != null){
+    fontSize = exampleText.style.fontSize;
+  }
+
+  // set text locations
+  sizeTitle.setAttribute('x', xStart);
+  minSizeText.setAttribute('x', xStart+textLength);
+  maxSizeText.setAttribute('x', xEnd-textLength);
+
+  // Set text sizes
+  sizeTitle.setAttribute('font-size', fontSize);
+  minSizeText.setAttribute('font-size', fontSize);
+  maxSizeText.setAttribute('font-size', fontSize);
+
+  // Set shape locations
+  circ1.setAttribute('cx', xStart + textLength + offset + 2);
+  circ2.setAttribute('cx', xEnd - textLength - offset - 7);
+
+  var triPoints = (xStart + textLength + 2*offset + 4) + ',35 ' + (xEnd-textLength-2*offset-14) +',41 ' + (xEnd-textLength-2*offset-14) + ',29';
+  tri.setAttribute('points', triPoints);
+
+}
+
+function getColumns(xValue, yValue, cValue, sValue, zValue){
+
+    var labels = 0;
+    var x = getColumnFromSelectorValue(xValue);
+    var y = getColumnFromSelectorValue(yValue);
+    var c = getColumnFromSelectorValue(cValue);
+    var s = getColumnFromSelectorValue(sValue);
+    var z = getColumnFromSelectorValue(zValue);
+
+    return cols = [labels, x, y, c, s, z];
+}
+
+function getColumnFromSelectorValue(selectorValue){
+  var column = null;
+  column = selectorValue;
+  return column;
+
+}
+
+function selectChange(){
+  reDraw = true;
+  drawBubbleChart();
+}
+
+// FILTER AND SEARCH
+// Table filter
+var oldColumns = [];
+function openFilterWindow(){
+  // Set columns of data table to match graph
+  var xValue = parseInt(xSelector.value); var yValue = parseInt(ySelector.value);
+  var cValue = parseInt(colorSelector.value); var sValue = parseInt(sizeSelector.value);
+  var zValue = parseInt(zSelector.value);
+
+  currentColumns = getColumns(xValue, yValue, cValue, sValue, zValue);
+  if (oldColumns.toString() != currentColumns.toString()){
+    tableView.setColumns(currentColumns);
+    dash.draw(tableView);
+  }
+  oldcolumns = currentColumns;
+
+  // Set rows of filter
+  if (filterRows.length == allRows.length){
+    filterView.setRows([]);
+  }else{
+    filterView.setRows(filterRows);
+  }
+
+  // Draw the filtered table with just the name column
+  filterView.setColumns([0]);
+  filterTable.draw(filterView);
+
+  // Finally, show the modal box
+  document.getElementById('filter-modal').style.display='block';
+}
+
+function applyFilter(){
+  document.getElementById('filter-modal').style.display='none';
+  drawBubbleChart();
+}
+
+function tableSelectAll(){
+  filterRows = allRows;
+  table.getChart().setSelection(selectAllArray);
+}
+
+function tableSelectNone(){
+  // Empty filter means plot all points on graph
+  filterRows = [];
+
+  // But show no points in filter column
+  filterView.setRows([]);
+  filterTable.draw(filterView);
+}
+
+function addToSelection(){
+  // Get selection from chart
+  var selection = table.getChart().getSelection();
+
+  // Add selection to filtered rows
+  for (i=0; i<selection.length; i++){
+    if (filterRows.indexOf(selection[i].row)<0){
+      filterRows.push( table.getDataTable().getTableRowIndex(selection[i].row) );
+    }
+  }
+
+  // Update filter table
+  filterView.setRows(filterRows);
+  filterTable.draw(filterView);
+
+  // Unselect rows in main data table
+  table.getChart().setSelection([]);
+}
+
+function removeFromSelection(){
+  // Grab selection in filter table.
+  var filterSelection = filterTable.getSelection();
+
+  // And remove it from filtered rows
+  for (i=0; i<filterSelection.length; i++){
+    filterRows.splice(filterSelection[i].row, 1);
+  }
+
+  // Update filter table
+  filterView.setRows(filterRows);
+  filterTable.draw(filterView);
+}
+
+// Graph selection
+function selectFromGraph(){
+  gettingPointsFromGraph = true;
+  graphFilterView.setColumns([0]);
+  graphFilterView.setRows(filterRows);
+  graphFilterTable.draw(graphFilterView);
+  document.getElementById("graph-filter-modal").style.display = "block";
+  document.getElementById('filter-modal').style.display='none';
+}
+
+function removeFromGraphSelection(){
+  var filterSelection = graphFilterTable.getSelection();
+  for (i=0; i<filterSelection.length; i++){
+    filterRows.splice(filterSelection[i].row, 1);
+  }
+    graphFilterView.setRows(filterRows);
+    graphFilterTable.draw(graphFilterView);
+}
+
+function applyGraphSelection(){
+  document.getElementById('graph-filter-modal').style.display='none';
+  gettingPointsFromGraph = false;
+  drawBubbleChart();
+}
+
+// Clicking on a point
+function pointSelected(data){
+  if (!relaying){
+    globalData = data; // debug purposes.
+    var selectedPoint = data.points[0].pointNumber;
+    var pointIndex = view.getTableRowIndex(selectedPoint);
+
+    if (gettingPointsFromGraph){
+      currentSelection = null;
+      // For filtering: add point to filtered rows array
+      if (filterRows.indexOf(pointIndex)<0){
+        filterRows.push(pointIndex);
+        graphFilterView.setRows(filterRows);
+        graphFilterTable.draw(graphFilterView);
+      }
+    } else {
+      // For annotation: select point for annotation
+      if (pointIndex == currentSelection){
+        currentSelection = null;
+      }else{
+        currentSelection = pointIndex;
+      }
+      drawBubbleChart();
+    }
+  } else {
+    relaying = false;
+  }
+}
+
+// PLAYBACK
+var playing = false;
+function playButtonClick(){
+  buttonHandle = document.getElementById('playButton');
+  if (!playing){
+    playing=true;
+    buttonHandle.value = 'pause';
+    playSequence();
+  } else {
+    playing=false;
+    buttonHandle.value = 'play_arrow';
+  }
+}
+
+// RESIZE
+window.addEventListener('resize', debounce(windowResizeFunction, 200));
+
+function windowResizeFunction(){
+  switch(document.getElementById('filter-modal').style.display){
+    case '':
+    case 'none':
+      reDraw = true;
+      drawBubbleChart();
+      break;
+    case 'block':
+      document.getElementById('graph-filter-modal').style.display='none';
+  }
+}
+
+// GENERIC DEBOUNCE FUNCTION
+function debounce(func, wait, immediate) {
+  // Debounce function from https://john-dugan.com/javascript-debounce/
+    var timeout;
+    return function() {
+        var context = this,
+            args = arguments;
+        var later = function() {
+            timeout = null;
+            if ( !immediate ) {
+                func.apply(context, args);
+            }
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait || 200);
+        if ( callNow ) {
+            func.apply(context, args);
+        }
+    };
+};
